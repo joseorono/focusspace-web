dff1db84aa8fd749ec7c833cd5d1eeaa7fc9beb4
Fernando Barrera <barrerafernando54@gmail.com>
2026-04-11 19:23:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@/components/Header'
 import { HeroSection } from '@/components/HeroSection'
 import { FeaturesSection } from '@/components/FeaturesSection'
+import { HowItWorksSection } from '@/components/HowItWorksSection'
+import { FaqSection } from '@/components/FaqSection'
 import { ReadySection } from '@/components/ReadySection'
 import { Footer } from '@/components/Footer'
 
@@ -11,6 +13,8 @@ function App() {
       <main className="flex flex-1 flex-col overflow-x-hidden">
         <HeroSection />
         <FeaturesSection />
+        <HowItWorksSection />
+        <FaqSection />
         <ReadySection />
       </main>
       <Footer />
