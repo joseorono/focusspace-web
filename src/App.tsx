21f312abff97f0337be0bff13ae27188adf96924
Fernando Barrera <barrerafernando54@gmail.com>
2026-04-11 18:04:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { TestView } from '@/components/TestView'
+import { Header } from '@/components/Header'
+import { HeroSection } from '@/components/HeroSection'
+import { FeaturesSection } from '@/components/FeaturesSection'
+import { ReadySection } from '@/components/ReadySection'
+import { Footer } from '@/components/Footer'
 
 function App() {
-  return <TestView />
+  return (
+    <>
+      <Header />
+      <main className="flex flex-1 flex-col overflow-x-hidden">
+        <HeroSection />
+        <FeaturesSection />
+        <ReadySection />
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
